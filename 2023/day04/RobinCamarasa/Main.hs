44f7b485423d5cdaa4da9428ab16556a1cb66ab9
@RobinCamarasa <robin.camarasa@gmail.com>
2023-12-04 10:25:04 +0100

    Add part 2 of day 4

--- 2023/day04/RobinCamarasa/Main.hs
+++ 2023/day04/RobinCamarasa/Main.hs
@@ -1,11 +1,24 @@
 import Data.List.Split as SPL
+
 data Card = Card Int [Int] [Int] deriving Show
 
+wonNumbers :: Card -> Int
+wonNumbers = length. listWonNumbers
+    where listWonNumbers (Card _ w p)= (filter (flip elem $ w)) $ p
+
 partOne :: String -> Int
-partOne puzzleInput = foldr (+) 0 wonNumbers
-    where cards = parseInput puzzleInput 
-          listWonNumbers (Card _ w p)= (filter (flip elem $ w)) $ p
-          wonNumbers = map ((`div` 2) . (2^). length . listWonNumbers) cards
+partOne = (foldr (+) 0) . getPoints
+    where getPoints = (map ((`div` 2) . (2^) . wonNumbers)) . parseInput
+
+partTwo :: String -> Int
+partTwo = fst . (foldl getNumberCards (0, [])) . (map wonNumbers) . parseInput
+    where getNumberCards :: (Int, [Int]) -> Int -> (Int, [Int])
+          getNumberCards (value, []) number = (value + 1, take number $ repeat 1)
+          getNumberCards (value, head:queue) number = (value + head + 1, update queue number (head + 1))
+          update :: [Int] -> Int -> Int -> [Int]
+          update list 0 _ = list
+          update [] n number = take n $ repeat number 
+          update (head:queue) n number = (head + number) : (update queue (n - 1) number)
 
 parseInput :: String -> [Card]
 parseInput = (map mkCard) . lines
@@ -24,3 +37,4 @@ main :: IO()
 main = do
     puzzleInput <- readFile "data/day04.txt"
     (putStr . ("Part one: " ++) . (++ "\n") . show . partOne) puzzleInput
+    (putStr . ("Part two: " ++) . (++ "\n") . show . partTwo) puzzleInput
