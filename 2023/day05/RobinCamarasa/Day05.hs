1674e1d03e87b38e8b670878e061e0c57cf5f6bd
@RobinCamarasa <robin.camarasa@gmail.com>
2023-12-05 11:14:33 +0100

    Add the initialisation with range

--- 2023/day05/RobinCamarasa/Day05.hs
+++ 2023/day05/RobinCamarasa/Day05.hs
@@ -1,3 +1,7 @@
+module Day05 where
+
+import Data.Maybe
+
 import Data.List.Split as SPL
 import qualified Data.Map as MAP
 
@@ -5,13 +9,18 @@ data Entry = Entry {dest :: Int, src :: Int, range :: Int} deriving (Eq, Show)
 data MapEntry = MapEntry {to :: String, entries :: [Entry]} deriving (Eq, Show)
 
 type Step = (String, Int)
+type StepRange = (String, (Int, Int))
 type Almanach = MAP.Map String MapEntry
 
-parseInput :: String -> ([Step], Almanach)
-parseInput puzzleInput = (seedValues, MAP.fromList . (map toKeyMapEntry) $ maps)
+-- parseInput :: String -> ([StepRange], [Step], Almanach)
+parseInput puzzleInput = (addSeed seedRanges, addSeed seedValues, MAP.fromList . (map toKeyMapEntry) $ maps)
     where (seeds:maps) = SPL.splitOn "\n\n" puzzleInput
           toInt value =  (read value :: Int)
-          seedValues = (zip (repeat "seed")) . (map toInt) . (SPL.splitOn " ") . (drop 7) $ seeds 
+          seedValues = (map toInt) . (SPL.splitOn " ") . (drop 7) $ seeds 
+          seedRanges = toRange seedValues 
+          toRange [] = []
+          toRange (a:b:q) = ((a,b):(toRange q))
+          addSeed = zip (repeat "seed")
           toKeyMapEntry mapValue = (from, MapEntry {to=to, entries=entriesValues})
               where (almaMapDesc:entries) = lines mapValue
                     (from:to:_) = (SPL.splitOn "-to-") . head . (SPL.splitOn " map:") $ almaMapDesc
@@ -28,16 +37,34 @@ getValue almanach step@(key, value)
                   | otherwise = -(dest + delta)
                       where delta = value - src
 
+getRange :: Almanach -> StepRange -> [StepRange]
+getRange almanach step@(key, (stepMin, stepRange))
+    | not . (MAP.member key) $ almanach = [step]
+    | otherwise = concat . (map (getRange almanach)) . catMaybes . (map update) $ entries
+        where (MapEntry {to=to, entries=entries}) = (almanach MAP.!) $ key
+              update (Entry {dest=destMin, src=entryMin, range=entryRange})
+                  | destRange < 0 = Nothing
+                  | otherwise = [Just (to, (leftBound, destRange))]
+                  where leftBound = max stepMin entryMin
+                        rightBound = min (stepMin + stepRange) (entryMin + entryRange)
+                        destBound = destMin + (leftBound - entryMin)
+                        destRange = rightBound - leftBound
+
+
 
 lowestFinalStep :: Almanach -> [Step] -> Int
 lowestFinalStep almanach = minimum . (map (snd . (getValue almanach)))
 
 partOne :: String -> Int
 partOne puzzleInput = lowestFinalStep almanach initSteps
-    where (initSteps, almanach) = parseInput puzzleInput
+    where (_, initSteps, almanach) = parseInput puzzleInput
+
+-- partTwo :: String -> [Step]
+partTwo puzzleInput = minimum . map fst . map snd . concat . (map (getRange almanach)) $ initStepRanges
+    where (initStepRanges, _, almanach) = parseInput puzzleInput
 
 main :: IO()
 main = do
     puzzleInput <- readFile "data/day05.txt"
     (putStr . ("Part one: " ++) . (++ "\n") . show . partOne) puzzleInput
-    (putStr . ("Part two: " ++) . (++ "\n") . show . partOne) puzzleInput
+    (putStr . ("Part two: " ++) . (++ "\n") . show . partTwo) puzzleInput
